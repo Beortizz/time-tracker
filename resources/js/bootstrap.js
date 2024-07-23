b46554877c6dc76881b2eca224506939f2ac9c32
Bernardo Ortiz <103218762+Beortizz@users.noreply.github.com>
2024-07-23 09:16:24 -0300

    Add BASE_URL env into bootstrap.js

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -4,6 +4,6 @@ window.axios = axios;
 window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';    
 window.axios.defaults.withCredentials = true;
 window.axios.defaults.withXSRFToken = true;
-window.axios.defaults.baseURL = "http://127.0.0.1:8000/api";
+window.axios.defaults.baseURL = process.env.VUE_APP_BASE_URL || "http://127.0.0.1:8000/api";
 
 export default axios;
