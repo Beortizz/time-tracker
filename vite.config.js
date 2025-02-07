9575e68f998901f7f9cd0804f609b818ac39ae26
Bernardo Ortiz <103218762+Beortizz@users.noreply.github.com>
2025-02-06 23:16:29 -0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,21 @@
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import laravel from 'laravel-vite-plugin';
 import vue from '@vitejs/plugin-vue';
 
-export default defineConfig({
-    plugins: [
-        laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
-            refresh: true,
-        }),
-        vue()
-    ],
-   define: {
-        'import.meta.env.VITE_APP_BASE_URL': JSON.stringify(process.env.VITE_APP_BASE_URL || 'http://127.0.0.1:8000/api')
-    }
-});
+export default ({ mode }) => {
+    // Carrega as variáveis de ambiente correspondentes ao modo atual
+    const env = loadEnv(mode, process.cwd());
+
+    return defineConfig({
+        plugins: [
+            laravel({
+                input: ['resources/css/app.css', 'resources/js/app.js'],
+                refresh: true,
+            }),
+            vue()
+        ],
+        define: {
+            'import.meta.env.VITE_APP_BASE_URL': JSON.stringify(env.VITE_APP_BASE_URL || 'http://127.0.0.1:8000/api')
+        }
+    });
+};
