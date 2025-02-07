43acb6136c0a860489602d9f18653528723f57d1
Bernardo Ortiz <103218762+Beortizz@users.noreply.github.com>
2025-02-06 23:04:41 -0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -12,7 +12,7 @@ export default defineConfig({
     ],
     define: {
         'process.env': {
-            VUE_APP_BASE_URL: process.env.VUE_APP_BASE_URL || 'http://127.0.0.1:8000/api'
+            VUE_APP_BASE_URL: process.env.VITE_APP_BASE_URL || 'http://127.0.0.1:8000/api'
         }
     }
 });
