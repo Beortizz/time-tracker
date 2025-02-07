4bc85815e386a83c0b6eb71d2077e52c1eaf27e5
Bernardo Ortiz <103218762+Beortizz@users.noreply.github.com>
2025-02-06 23:31:44 -0300

    Update bootstrap.js

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -4,6 +4,6 @@ window.axios = axios;
 window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';    
 window.axios.defaults.withCredentials = true;
 window.axios.defaults.withXSRFToken = true;
-window.axios.defaults.baseURL = process.env.VITE_APP_BASE_URL;
+window.axios.defaults.baseURL = import.meta.env.VITE_APP_BASE_URL;
 
 export default axios;
